ddce1af881e0e6ecf314444a077312ded31919d5
Ioana <coruga.ioana@gmail.com>
2020-04-27 17:19:09 +0100

    Getting Started With Angular - install and bootstrap settings- include styles and scripts

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `
+  <h1>Hello from events-app.component.ts </h1>
+  <img src="/assets/images/basic-shield.png"/> 
+  <!-- this path is relative to the index.html file-->
+  <!-- this path is recognised becasue in the angular.json file we have declared the "assets" path under architect: options: "assets": [
+    "src/favicon.ico",
+    "src/assets"
+  ],
+  
+  General styles are delared here as well, vs component deplared styles which apply just to the component (?? and )
+  
+  -->
+  `
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
