9a44c8868a5aeb717173e6ee2e1dd43111f32032
Ioana <coruga.ioana@gmail.com>
2020-05-02 13:24:47 +0100

    Communicating with Child/Parent Component - @Input and @Output properties

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -5,10 +5,31 @@ import { Component } from '@angular/core'
     template: `
     <div>
         <h1>Upcoming Angular Events</h1>
-    </div>
+        <hr/>
+        <event-thumbnail (eventClick)="handleClickInChild($event)" [event]="event1" ></event-thumbnail>
+            <!-- This (eventClick) binds to the @Output property in the child component <event-thumbnail>;
+            the @Output eventClick property is an event so we bind with () not with []
+            -->
+        </div>
     `
 })
 export class EventsListComponent {
 
-
+    event1 = {
+        id: 1, 
+        name: 'Angular Connect',
+        date: '9/26/2036',
+        time: '10:00 am',
+        price: 599.99,
+        imageUrl: '/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1057 DT',
+            city: 'London',
+            country: 'England'
+        }
+    }
+    
+    handleClickInChild(data) {
+        console.log('in parent event received: ' + data)
+    }
 }
