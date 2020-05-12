839b8349f662316f90a538a667da14160ab55250
Ioana <coruga.ioana@gmail.com>
2020-05-12 17:34:43 +0100

    Register a third party service as a Opaque Token - InjectionToken - so to not to have to have a service  wrapper around the third party service;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,8 @@ import { EventsListComponent,
   SessionListComponent,
   DurationPipe} from './events/index'
 import { NavBarComponent } from './nav/navbar.component'
-import { ToastrService } from './common/toastr.service'
+// import { ToastrService } from './common/toastr-with-wrapper.service'
+import { TOASTR_TOKEN, IToastr} from './common/toastr.service'
 import { CollapsibleWellComponent } from './common/collapsible-well.component'
 import { Error404Component } from './errors/404.component'
 import { appRoutes } from './routes'
@@ -22,6 +23,9 @@ import { AuthService } from './user/auth.service'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { CollapsibleWellWitSlots as CollapsibleWellWithSlots } from './common/collapsible-well-with-slots.component'
 import { CollapsibleWellWithThreeSlots } from './common/collapsible-well-three-slots.component'
+
+//this tells Typescript thet we know there is a global variable created by importing the /toast.min.js script, so no need to worry about it
+declare let toastr: IToastr
 
 @NgModule({
   imports: [
@@ -48,7 +52,8 @@ import { CollapsibleWellWithThreeSlots } from './common/collapsible-well-three-s
   //register service with angular so Angular will provide an instance of the object where the service is injected
   providers: [
     EventService, 
-    ToastrService,
+    // ToastrService,
+    { provide: TOASTR_TOKEN, useValue: toastr },
     EventRouteActivator, //EventRouteActivator this is the short hand form for declaring a provider
     //register afunction as a provider in a module, usign the long hand syntax for deplaring a provider
     //when <requested> use <service>
--- src/app/common/toastr.service.ts
+++ src/app/common/toastr.service.ts
@@ -1,24 +1,10 @@
-import { Injectable } from '@angular/core'
+import { InjectionToken } from '@angular/core'
 
-//when we imported into the ancular CLI configurration aka angular.json file the toastr files, this made toastr globally available, 
-//but typescript does not know that, so here we let know typescript that toastr is something we know about 
-declare let toastr:any
+export let TOASTR_TOKEN = new InjectionToken<IToastr>('toastr')
 
-@Injectable()
-export class ToastrService {
-    success (message:string, title?:string){
-        toastr.success(message, title)
-    }
-
-    error (message:string, title?:string){
-        toastr.error(message, title)
-    }
-
-    info (message:string, title?:string){
-        toastr.info(message, title)
-    }
-
-    warn (message:string, title?:string){
-        toastr.console.warn(message, title)
-    }
+export interface IToastr {
+    success (message:string, title?:string): void
+    error (message:string, title?:string): void
+    info (message:string, title?:string): void
+    warn (message:string, title?:string): void
 }
