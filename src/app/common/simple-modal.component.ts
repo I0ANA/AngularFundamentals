f7c979037497ec4cf0d4cd8be95c5029eaec131f
Ioana <coruga.ioana@gmail.com>
2020-05-14 18:23:44 +0100

    Expose settings for the simple-modal like closeOnBodyClick

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,4 +1,5 @@
-import {Component, Input } from   '@angular/core'
+import { Component, Input, ViewChild, ElementRef, Inject } from   '@angular/core'
+import { JQUERY_TOKEN } from './jQuery.service'
 
 @Component({
     selector:'simple-modal',
@@ -9,8 +10,9 @@ import {Component, Input } from   '@angular/core'
     //closing button has a close class and a data-dismiss attribute to close the modal, becasue of these classes the button will be shifted to the right
     //&times; is the x on the button
 
+    //the template reference variable #modalcontainer can't have - in the name 
     template:`
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -19,7 +21,7 @@ import {Component, Input } from   '@angular/core'
                     </button>
                     <h4 class="modal-title">{{title}}</h4>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body" (click)="closeModal()">
                     <ng-content></ng-content>
                 </div>
             </div>
@@ -33,4 +35,17 @@ import {Component, Input } from   '@angular/core'
 export class SimpleModalComponent {
     @Input() title: string
     @Input() elementId: string
+    @Input() closeOnBodyClick: boolean
+    //instead of injecting the ElementRef in the constructor, 
+    //we can use a ViewChild to access the template reference variable
+    //we can add the #reference on whichever node we want to wrap, we want the wrapper div node 
+    @ViewChild('modalcontainer') containerEl:ElementRef
+
+    constructor(@Inject(JQUERY_TOKEN) private $:any){}
+
+    closeModal() {
+        if(this.closeOnBodyClick){
+            this.$(this.containerEl.nativeElement).modal('hide')
+        }
+    }
 }
