5e8f54292c0c3c2116d194b2a037691a24c8f372
Ioana <coruga.ioana@gmail.com>
2020-05-15 19:00:28 +0100

    the guard that checks if an event exists and redirects to 404 if not is broken becasue now getEvent returns an observable - EventRouteActivator no longer needed - we will preload the data using a resolver

--- src/app/events/event-details/event-route-activator.service.ts
+++ src/app/events/event-details/event-route-activator.service.ts
@@ -3,20 +3,22 @@ import { Injectable }  from '@angular/core'
 import { EventService } from '../shared/event.service'
 
 @Injectable()
-export class EventRouteActivator implements CanActivate {
+export class EventRouteActivator 
+//implements CanActivate
+ {
 
-    constructor(private eventService:EventService, private router:Router){
-    }
+    // constructor(private eventService:EventService, private router:Router){
+    // }
 
-    canActivate(route: ActivatedRouteSnapshot) {
+    // canActivate(route: ActivatedRouteSnapshot) {
 
-        //cast to boolean using !!
-        const eventExists = !!this.eventService.getEvent(+route.params['id'])
+    //     //cast to boolean using !!
+    //     const eventExists = !!this.eventService.getEvent(+route.params['id'])
 
-        if(!eventExists)
-            this.router.navigate(['/404'])
+    //     if(!eventExists)
+    //         this.router.navigate(['/404'])
 
-        //we need to return the guard result, if not exist then redirect, otherwise return true
-        return eventExists
-    }
+    //     //we need to return the guard result, if not exist then redirect, otherwise return true
+    //     return eventExists
+    // }
 }
